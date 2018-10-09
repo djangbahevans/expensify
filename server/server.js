4506fb858b12d95382ac5b6242b7df07373d4320
Evans Djangbah <djangbahevans@yahoo.com>
2018-10-09 05:02:05 +0000

    Setup production build and server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+
+app.use(express.static(publicPath));
+
+app.get('*', (request, response) => {
+    response.sendFile(path.join(publicPath, 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {console.log('Server is up.')});
